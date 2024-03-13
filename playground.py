f240361db9e63510fc452a198caeec8aad633863
dagdomdagdom <163330826+dagdomdagdom@users.noreply.github.com>
2024-03-13 17:05:55 +0000

    initial commit

--- playground.py
+++ playground.py
@@ -1,1 +1,14 @@
 
+# Variables
+name = "Alice"
+age = 25
+
+# If statement
+if age >= 18:
+    print(f"{name} is an adult")
+else:
+    print(f"{name} is a minor")
+
+# For loop
+for i in range(5):
+    print(i)
